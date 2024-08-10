c9ae73ad9faa19e6132d1d2690c849020f7f9dda
Rifat Shikder <rifat.shikder@tutors.es>
2024-08-10 22:36:43 +0600

    fix build error

--- src/components/section/Hero.jsx
+++ src/components/section/Hero.jsx
@@ -1,63 +1,63 @@
-"use client";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import RiButton from "../buttons/RiButton";
+'use client';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import RiButton from '../buttons/RiButton';
 
 function Hero() {
   const router = useRouter();
   function gotoSignup() {
-    router.push("/signup");
+    router.push('/signup');
   }
   return (
-    <section className="rounded-ss-full bg-gray-200 pt-24 md:pt-0">
-      <div className="min-h-screen container md:flex items-center gap-4 h-full ">
-        <div className="flex-1 flex flex-col gap-8">
-          <h1 className="text-6xl">
+    <section className='pt-24 bg-gray-200 rounded-ss-full md:pt-0'>
+      <div className='container items-center h-full min-h-screen gap-4 md:flex '>
+        <div className='flex flex-col flex-1 gap-8'>
+          <h1 className='text-6xl'>
             Donate blood, <br />
             save life!
           </h1>
           <em>
-            "Many people owe their lives to selfless blood donors," said WHO
+            Many people owe their lives to selfless blood donors, said WHO
             Regional Director for the Western Pacific, Dr Shin Young-soo.
-            "Voluntary blood donors come from all walks of life, but they share
-            a common concern for the welfare of others. We thank them for their
-            admirable generosity and concern for those in need."
+            Voluntary blood donors come from all walks of life, but they share a
+            common concern for the welfare of others. We thank them for their
+            admirable generosity and concern for those in need.
           </em>
 
-          <RiButton text="Donate Now" onClick={gotoSignup} />
+          <RiButton text='Donate Now' onClick={gotoSignup} />
         </div>
 
-        <div className="flex-1 mt-10 md:mt-0">
-          <div className="w-full h-[400px] bg-red-500 rounded-full rounded-es-none overflow-hidden">
+        <div className='flex-1 mt-10 md:mt-0'>
+          <div className='w-full h-[400px] bg-red-500 rounded-full rounded-es-none overflow-hidden'>
             <Image
-              src="/img/blood_donation.jpg"
-              alt="blood donation"
+              src='/img/blood_donation.jpg'
+              alt='blood donation'
               width={600}
               height={400}
-              className="w-full object-cover"
+              className='object-cover w-full'
             />
           </div>
         </div>
       </div>
 
-      <div className=" mt-20 md:mt-0 bg-red-500 rounded-se-full relative overflow-hidden">
-        <div className="w-80 h-80 rounded-full bg-red-400/50 absolute top-5 left-5" />
+      <div className='relative mt-20 overflow-hidden bg-red-500  md:mt-0 rounded-se-full'>
+        <div className='absolute rounded-full w-80 h-80 bg-red-400/50 top-5 left-5' />
 
-        <div className="relative container py-10 md:flex justify-between items-center gap-10">
-          <div className="text-white">
-            <h2 className="text-xl md:text-2xl">
+        <div className='container relative items-center justify-between gap-10 py-10 md:flex'>
+          <div className='text-white'>
+            <h2 className='text-xl md:text-2xl'>
               We are helping people from 10 years
             </h2>
-            <p className="text-sm mt-4 ">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br />{" "}
+            <p className='mt-4 text-sm '>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br />{' '}
               Animi magnam autem consectetur dolor. In laudantium corporis
               fugiat est sint officiis.
             </p>
           </div>
           <RiButton
-            text="Donate Now"
+            text='Donate Now'
             onClick={gotoSignup}
-            className="mr-20 mt-5 md:mt-0"
+            className='mt-5 mr-20 md:mt-0'
           />
         </div>
       </div>
